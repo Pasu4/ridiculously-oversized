bbfa05c3dfd0f991fa0ef7ab847dd25ec6ad3538
Pasu4 <117922538+Pasu4@users.noreply.github.com>
2023-03-12 12:11:10 +0100

    I really should have tested this

--- scripts/unhide.js
+++ scripts/unhide.js
@@ -5,22 +5,25 @@ const scrapWalls = [
     Blocks.scrapWallGigantic
 ];
 
-// Make scrap walls buildable
-scrapWalls.forEach(function (value) {
-    value.buildVisibility = BuildVisibility.shown;
-});
+exports.unhide = () =>
+{
+    // Make scrap walls buildable
+    scrapWalls.forEach(function (value) {
+        value.buildVisibility = BuildVisibility.shown;
+    });
 
-// Add to tech tree
-TechTree.all.find(boolf(t => t.content.name.equals(Blocks.copperWallLarge))).children.add(
-    TechTree.node(Blocks.scrapWall, () => {
-        TechTree.node(Blocks.scrapWallLarge, () => {
-            TechTree.node(Blocks.scrapWallHuge, Seq.with(new Objectives.SectorComplete(Vars.content.sector("ancient-outpost"))), () => {
-                TechTree.node(Blocks.scrapWallGigantic, () => { });
+    // Add to tech tree
+    TechTree.all.find(boolf(t => t.content.name.equals(Blocks.copperWallLarge))).children.add(
+        TechTree.node(Blocks.scrapWall, () => {
+            TechTree.node(Blocks.scrapWallLarge, () => {
+                TechTree.node(Blocks.scrapWallHuge, Seq.with(new Objectives.SectorComplete(Vars.content.sector("ancient-outpost"))), () => {
+                    TechTree.node(Blocks.scrapWallGigantic, () => { });
+                });
             });
-        });
-    })
-);
+        })
+    );
 
-// Make Shard core visible if Chip core is researched
-if(Blocks.coreShard.unlocked)
-    Blocks.coreShard.buildVisibility = BuildVisibility.shown;
+    // Make Shard core visible if Chip core is researched
+    if(Vars)
+        Blocks.coreShard.buildVisibility = BuildVisibility.shown;
+};
